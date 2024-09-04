8e8af72884ce5ed4e91927c016eb0c9c9c42cc82
DarkSky <darksky2048@gmail.com>
2024-09-04 14:42:58 +0800

    feat: make it crash

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,13 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .register_asynchronous_uri_scheme_protocol("http", |_, _, resp| {
+            resp.respond(
+                tauri::http::Response::builder()
+                    .body("test".as_bytes().to_vec())
+                    .expect("failed to generate response"),
+            );
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
